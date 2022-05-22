5770fcd7e5516b1a05664a884e89c804cf8dd4ff
Md Ishaque <iamishaque.business@gmail.com>
2022-05-22 10:29:24 +0600

    set up create product api

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require('dotenv').config();
 
 const app = express()
@@ -8,6 +9,33 @@ const port = process.env.PORT || 5000
 // middleware
 app.use(cors())
 app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lh8h8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+async function run() {
+    try {
+      await client.connect();
+      const productCollection = client.db("speed_battery_manufacturer").collection("products");
+   
+  
+      /************************************ Product api *********************************************/
+      app.post('/product',async (req,res)=>{
+        const product = req.body
+        await productCollection.insertOne(product)
+        res.send({success:true,message:`${product.name} is ready to sell!`})
+      })
+      
+  
+  
+  
+    } finally {
+      // await client.close();
+    }
+  }
+  run().catch(console.dir);
 
 
 app.get('/',(req,res)=>{
