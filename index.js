3bb65132ce8c50627a7d21ac8fa9f0d8e9dd0060
Md Ishaque <iamishaque.business@gmail.com>
2022-05-23 02:29:26 +0600

    create product grt by id api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require('dotenv').config();
 
 const app = express()
@@ -10,38 +10,54 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lh8h8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ajeexbn.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
 
 async function run() {
-    try {
-      await client.connect();
-      const productCollection = client.db("speed_battery_manufacturer").collection("products");
-   
-  
-      /************************************ Product api *********************************************/
-      app.post('/product',async (req,res)=>{
-        const product = req.body
-        await productCollection.insertOne(product)
-        res.send({success:true,message:`${product.name} is ready to sell!`})
-      })
-      
-  
-  
-  
-    } finally {
-      // await client.close();
-    }
+  try {
+    await client.connect();
+    const productCollection = client.db("speed_battery_manufacturer").collection("products");
+    const ordersCollection = client.db("speed_battery_manufacturer").collection("orders");
+
+
+    /************************************ Product api *********************************************/
+    app.get('/product/:productID', async (req, res) => {
+      const productID = req.params.productID
+      const result = await productCollection.findOne({ _id: ObjectId(productID) })
+      res.send(result)
+    })
+    app.get('/product', async (req, res) => {
+      const result = await productCollection.find({}).toArray()
+      res.send(result)
+    })
+    app.post('/product', async (req, res) => {
+      const product = req.body
+      await productCollection.insertOne(product)
+      res.send({ success: true, message: `${product.name} is ready to sell!` })
+    })
+
+
+    /************************************ Product api *********************************************/
+    app.post('/order', async (req, res) => {
+      const product = req.body
+      await ordersCollection.insertOne(product)
+      res.send({ success: true})
+    })
+
+
+  } finally {
+    // await client.close();
   }
-  run().catch(console.dir);
+}
+run().catch(console.dir);
 
 
-app.get('/',(req,res)=>{
-    res.send("Hello Mom")
+app.get('/', (req, res) => {
+  res.send("Hello Mom")
 })
 
-app.listen(port,()=>{
-    console.log(`Listening on port ${port}`)
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
 })
