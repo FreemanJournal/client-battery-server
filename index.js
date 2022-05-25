40e65d8316e5dac5405b07a6afc39a68d94b1838
Md Ishaque <iamishaque.business@gmail.com>
2022-05-26 02:29:23 +0600

    create isAdmin api

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require('dotenv').config();
+var jwt = require('jsonwebtoken');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const app = express()
 const port = process.env.PORT || 5000
@@ -13,6 +14,22 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ajeexbn.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJwt(req, res, next) {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ message: "Unauthorized access." })
+  }
+  const authToken = authorization.split(' ')[1]
+  jwt.verify(authToken, process.env.JWT_ACCESS_TOKEN, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden Access" });
+    }
+    req.decoded = decoded;
+    next();
+  })
+
+}
+
 
 
 async function run() {
@@ -21,14 +38,97 @@ async function run() {
     const productCollection = client.db("speed_battery_manufacturer").collection("products");
     const ordersCollection = client.db("speed_battery_manufacturer").collection("orders");
     const paymentCollection = client.db("speed_battery_manufacturer").collection("payments");
+    const userCollection = client.db("speed_battery_manufacturer").collection("users");
+    const reviewCollection = client.db("speed_battery_manufacturer").collection("reviews");
+
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const user = await userCollection.findOne({ email })
+      // console.log('isAdmin',!user.isAdmin);
+      if (!user.isAdmin) {
+        return res.status(403).send({ message: "Forbidden Access." })
+      }
+      next();
+    }
 
 
+    /************************************ Create Review api *********************************************/
+    app.get('/review', async (req, res) => {
+      const result = await reviewCollection.find({}).sort({ $natural: -1 }).toArray()
+      res.send(result)
+    })
+    app.post('/review', verifyJwt, async (req, res) => {
+      const review = req.body
+      await reviewCollection.insertOne(review)
+      res.send({ success: true })
+    })
+    /************************************ User api *********************************************/
+
+    app.delete("/user/:email", verifyJwt,verifyAdmin, async (req, res) => {
+      const { email } = req.params
+      const result = await userCollection.deleteOne({ email });
+      if (result.deletedCount > 0) {
+        res.send({ success: true })
+      }
+
+    })
+    app.get("/user", verifyJwt,verifyAdmin, async (req, res) => {
+      const result = await userCollection.find({}).sort({ $natural: -1 }).toArray();
+      res.send(result)
+
+    })
+    app.get("/user/:email", async (req, res) => {
+      const { email } = req.params
+      const result = await userCollection.findOne({ email })
+      // console.log(object);
+      res.send(result)
+
+    })
+    app.get("/isAdmin/:email", async (req, res) => {
+      const { email } = req.params
+      const user = await userCollection.findOne({ email })
+      if (!!user?.isAdmin) {
+        return res.send({ success: true })
+      }else{
+        return res.send({ success: false })
+      }
+
+    })
+    app.put("/user/admin", verifyJwt,verifyAdmin, async (req, res) => {
+      const { email, status } = req.body
+      const result = await userCollection.updateOne({ email }, { $set: { isAdmin: status } }, { upsert: true });
+      if (result.modifiedCount > 0) {
+        res.send({ success: true })
+      }
+    })
+    app.put("/user/update", verifyJwt, async (req, res) => {
+      const user = req.body
+      user && delete user._id
+      const result = await userCollection.updateOne({ email: user.email }, { $set: user }, { upsert: true });
+      if (result.modifiedCount > 0) {
+        res.send({ success: true })
+      }
+    })
+    app.post("/user", async (req, res) => {
+      const user = req.body
+      user && delete user?._id
+      const result = await userCollection.updateOne({ email: user.email }, { $set: user }, { upsert: true });
+      if (result.modifiedCount > 0) {
+        res.send({ success: true })
+      }
+    })
+    /************************************ Create Token api *********************************************/
+    app.post("/createToken", async (req, res) => {
+      const {email} = req.body
+      const authAccessToken = jwt.sign({email}, process.env.JWT_ACCESS_TOKEN, { expiresIn: "1d" })
+      res.send({ authAccessToken })
+    })
     /************************************ Shipping api *********************************************/
-    app.patch('/shipping/:productID', async (req, res) => {
+    app.patch('/shipping/:productID', verifyJwt,verifyAdmin, async (req, res) => {
       const productID = req.params.productID
       const updateDoc = {
         $set: {
-          isShipped:true
+          isShipped: true
         }
       }
       await ordersCollection.updateOne({ _id: ObjectId(productID) }, updateDoc);
@@ -36,7 +136,7 @@ async function run() {
 
     })
     /************************************ Payment api *********************************************/
-    app.patch('/payment/:productID', async (req, res) => {
+    app.patch('/payment/:productID', verifyJwt, async (req, res) => {
       const productID = req.params.productID
       const payment = req.body
       const updateDoc = {
@@ -51,7 +151,7 @@ async function run() {
 
     })
     /************************************ Stripe api *********************************************/
-    app.post('/create-payment-intent', async (req, res) => {
+    app.post('/create-payment-intent', verifyJwt, async (req, res) => {
       const total = req.body.total
       const amount = total * 100
       const paymentIntent = await stripe.paymentIntents.create({
@@ -62,59 +162,64 @@ async function run() {
       res.send({ clientSecret: paymentIntent.client_secret })
     })
     /************************************ Order api *********************************************/
-    app.delete('/order/:productID', async (req, res) => {
+    app.delete('/order/:productID', verifyJwt, async (req, res) => {
       const productID = req.params.productID
       await ordersCollection.deleteOne({ _id: ObjectId(productID) })
-      res.send({success:true,message:"Your order deleted successfully."})
+      res.send({ success: true, message: "Your order deleted successfully." })
     })
-    app.get('/order/:productID', async (req, res) => {
+    app.get('/order/:productID', verifyJwt, async (req, res) => {
       const productID = req.params.productID
       const result = await ordersCollection.findOne({ _id: ObjectId(productID) })
       res.send(result)
     })
-    app.get('/order', async (req, res) => {
-      const result = await ordersCollection.find({}).sort({$natural:-1}).toArray()
+
+    app.get('/myOrder/:email', verifyJwt, async (req, res) => {
+      const { email } = req.params
+      const result = await ordersCollection.find({ email }).sort({ $natural: -1 }).toArray()
       res.send(result)
     })
-    app.post('/order', async (req, res) => {
+    app.get('/order', verifyJwt,verifyAdmin, async (req, res) => {
+      const result = await ordersCollection.find({}).sort({ $natural: -1 }).toArray()
+      res.send(result)
+    })
+
+    app.post('/order', verifyJwt, async (req, res) => {
       const product = req.body
       await ordersCollection.insertOne(product)
       res.send({ success: true })
     })
 
     /************************************ Product api *********************************************/
-    app.delete('/product/:productID', async (req, res) => {
+    app.delete('/product/:productID', verifyJwt,verifyAdmin, async (req, res) => {
       const { productID } = req.params
       await productCollection.deleteOne({ _id: ObjectId(productID) });
       res.send({ success: true, message: `Product Successfully Deleted` })
     })
-    app.put('/product', async (req, res) => {
+    app.put('/product', verifyJwt,verifyAdmin, async (req, res) => {
       const product = req.body
       const result = await productCollection.updateOne({ _id: ObjectId(product.id) }, { $set: product });
       res.send({ success: true, message: `${product.name} is updated` })
     })
-    app.get('/product/:productID', async (req, res) => {
+    app.get('/product', async (req, res) => {
+      const result = await productCollection.find({}).sort({ $natural: -1 }).limit(3).toArray()
+      res.send(result)
+    })
+    app.get('/product/:productID', verifyJwt, async (req, res) => {
       const productID = req.params.productID
       const result = await productCollection.findOne({ _id: ObjectId(productID) })
       res.send(result)
     })
 
-    app.get('/product', async (req, res) => {
-      const result = await productCollection.find({}).sort({ $natural: -1 }).limit(3).toArray()
+
+    app.get('/product_all', verifyJwt,verifyAdmin, async (req, res) => {
+      const result = await productCollection.find({}).project({ image: 0, description: 0 }).sort({ $natural: -1 }).toArray()
       res.send(result)
     })
-    app.get('/product_all', async (req, res) => {
-      const result = await productCollection.find({}).sort({ $natural: -1 }).toArray()
-      res.send(result)
-    })
-    app.post('/product', async (req, res) => {
+    app.post('/product', verifyJwt,verifyAdmin, async (req, res) => {
       const product = req.body
       await productCollection.insertOne(product)
       res.send({ success: true, message: `${product.name} is ready to sell!` })
     })
-
-
-
 
 
   } finally {
